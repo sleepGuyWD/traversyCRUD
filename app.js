04970fdd8c3729f56cafbfef8c844b30fa971bdd
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-05-16 20:37:57 -0700

    Google outh, materialize, started passport.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const PORT = process.env.PORT || 3000
 
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -26,11 +32,29 @@ app.engine('.hbs', exphbs.engine({
 )
 app.set('view engine', '.hbs')
 
+// Sessions middleware
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 
+  'public')))
+
 // Routes
 app.use('/', require('./routes/index'))
-//!!! Ended Mayan Wolfe's stream @ 3:16:24
+
 
 app.listen(
   PORT, 
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
+
+//Continue from 4:50:00, Mayan Wolfe's stream
